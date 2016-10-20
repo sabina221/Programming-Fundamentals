da00d3aafd66d2705a0a50a789cdaaa51fc08c6f
sabina221 <sabinaprajapati221@gmail.com>
2016-10-20 12:30:50 +0545

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,31 @@
+/*
+name:sabina prajapati
+roll no:
+lab no:
+program:WAP to use the operators
+date:
+*/
+
+#include<stdio.h>
+  int main(){
+	 int x,y;
+	 printf("enter the value of x\n");
+	 scanf("%d",&x);
+	 printf("enter the value of y\n");
+	 scanf("%d",&y);
+	
+	 printf("use of operator ==  is: %d\n", x==y);
+	 
+	 printf("use of operator > is:%d\n",x>y);
+	 
+	 printf("use of operator < is:%d\n",x<y);
+	
+	 printf("use of operator != is:%d\n",x!=y);
+	 
+	 printf("use of operator >=  is: %d\n",x>=y);
+	
+	 printf("use of operator <= is: %d\n",x<=y);
+	 return 0;
+	 }
+	 	 
+	
