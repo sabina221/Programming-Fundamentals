56d05416577c3eea9e35fb3471f0a288ea1c77ac
sabina221 <sabinaprajapati221@gmail.com>
2016-10-20 12:07:03 +0545

    Add files via upload

---
+++ assignment operator.c
@@ -0,0 +1,40 @@
+/*
+students name :sabina prajapati
+subject :programming fundamental
+roll no:
+lab no:
+program:wap to use assignment operator
+date:
+
+*/
+#include<stdio.h>
+  int main(){
+	 int x,y;
+	 printf("enter the value of x\n");
+	 scanf("%d",&x);
+	 printf("enter the value of y\n");
+	 scanf("%d",&y);
+	 x+=y;
+	 printf("use of operator + is: %d\n",x);
+	 x-=y;
+	 printf("use of operator - is:%d\n",x);
+	 x*=y;
+	 printf("use of operator * is:%d\n",x);
+	x/=y;
+	 printf("use of operator / is:%d\n",x);
+	 x%=y;
+	 printf("use of operator %% is: %d\n",x);
+	 
+	 
+	 
+	
+	 
+	 
+	 
+	 return 0;
+	 
+ }
+	 
+	 
+	
+
---
+++ untitled.c
@@ -0,0 +1,25 @@
+/*
+ Student name : Sarik Shrestha
+ Subject      :Programming fundamentals
+ Roll no      :
+ Lab no       :
+ Program      :Print your name
+ Date         :2016/10/17
+ */
+ 
+ 
+#include<stdio.h>
+ 
+ void main() 
+ {
+ int n;
+ printf("enter any number");
+ scanf("%d",&n);
+ if(n>0){
+	 printf("it is positive num");
+	 }
+else
+{
+	printf("it is negative");
+	}
+}
