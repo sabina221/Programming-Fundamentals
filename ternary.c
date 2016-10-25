748a1dd882d7f5b7af7f8d1a4bc07e863003b150
sabina221 <sabinaprajapati221@gmail.com>
2016-10-25 10:49:16 +0545

    Add files via upload

--- ternary.c
+++ ternary.c
@@ -1,17 +1,1 @@
-/*
-student name:Sabina prajapati
-subject:Programming fundamental
-Lab no:
-Program:write a program to take two inputs from user and print largest and smallest numbers using terminjal operstors
-Date:24th october
-*/
 
-#include<stdio.h>
-main(){
-	int x,even,odd;
-	printf("enter the value of x\n");
-	scanf("%d",&x);
-(x%2==0)? printf("%d is even",x): printf("%d is odd",x);
-
-	
-}
