048c1d0946f2b042db1b95e4b12d269dae1717d6
sabina221 <sabinaprajapati221@gmail.com>
2016-11-14 10:39:04 +0545

    Add files via upload

---
+++ name2.c
@@ -0,0 +1,18 @@
+/*Name:Sabina Prajapati
+Subject:Programming Fundamental
+Program:Write a C program to print your name given by user
+Roll No:2059
+Lab Sheet No:1
+Date:Nov 14,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+char main(){
+char name[50];
+printf("write your name\n");
+scanf("%c",&name);
+getch();
+return 0;
+
+}
+
